52538459dd293f7d29acecc5be7f7c3e6a625ee5
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-07-15 01:10:28 +0600

    Create index.ts

---
+++ src/interfaces/index.ts
@@ -0,0 +1,4 @@
+export { TrackGet } from './track.get';
+export { TrackSearch } from './track.search';
+export { ChartArtists } from './chart.artitsts';
+export { ChartTracks } from './chart.tracks';
